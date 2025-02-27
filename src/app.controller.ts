7d5d2ae475be6d84370e73a4377b06587dbc743d
jamby77 <jamby77@gmail.com>
2025-02-27 18:39:29 +0200

    add redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('/info')
+  getInfo() {
+    return this.appService.getDbInfo();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
+import { createClient, RedisClientType } from 'redis';
+import redisConfig from './config/redis';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  client: RedisClientType;
+  constructor() {
+    const config = redisConfig();
+    this.client = createClient(config);
+    this.client.on('error', (err) => console.log('Redis Client Error', err));
+  }
+  private async getClient() {
+    await this.client.connect();
+    return this.client;
+  }
+
+  async getDbInfo() {
+    const client = await this.getClient();
+    const info = await client.info();
+    return info;
+  }
+
+  async getHello(): Promise<string | null> {
+    const client = await this.getClient();
+    await client.SET('hello', 'world');
+    return client.GET('hello');
   }
 }
