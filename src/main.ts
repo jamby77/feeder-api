a41abf874317122be58746ee2f4b62800cf4350c
jamby77 <jamby77@gmail.com>
2025-03-02 16:10:27 +0200

    remove class export

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { Response } from "express";
 import { ZodError } from "zod";
 
 @Catch(ZodError)
-export class ZodFilter<T extends ZodError> implements ExceptionFilter {
+class ZodFilter<T extends ZodError> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
