9b8801bd85b90259eb3294dcd64c5e9a7f992b6c
jamby77 <jamby77@gmail.com>
2025-03-02 21:03:10 +0200

    use custom origin function

--- src/main.ts
+++ src/main.ts
@@ -23,11 +23,20 @@ class ZodFilter<T extends ZodError> implements ExceptionFilter {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.PORT || "3000", 10);
-  const origin = process.env.ALLOWED_ORIGINS?.split("|") || ["http://localhost:3000"];
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split("|") || ["http://localhost:3000"];
   console.log({ origin });
   app.enableCors({
-    origin,
+    origin: function (origin: string, callback: (...args: unknown[]) => void) {
+      console.log({ origin, allowedOrigins });
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe",
   });
   app.useGlobalFilters(new ZodFilter());
   await app.listen(port);
