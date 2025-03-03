b0cd5dcae587075476e2473524364f60b36b45aa
jamby77 <jamby77@gmail.com>
2025-03-03 11:21:01 +0200

    change logger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from "@nestjs/common";
+import { ArgumentsHost, Catch, ConsoleLogger, ExceptionFilter, HttpStatus } from "@nestjs/common";
 import { Response } from "express";
 import { ZodError } from "zod";
 import * as process from "node:process";
@@ -21,12 +21,20 @@ class ZodFilter<T extends ZodError> implements ExceptionFilter {
 }
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      prefix: "Feeder-Api",
+      timestamp: true,
+      breakLength: 200,
+      colors: process.env.NODE_ENV === "development",
+      json: true, //process.env.NODE_ENV !== "development",
+    }),
+  });
   const port = parseInt(process.env.PORT || "3000", 10);
   const allowedOrigins = process.env.ALLOWED_ORIGINS?.split("|") || ["http://localhost:3000"];
   app.enableCors({
     origin: function (origin: string, callback: (...args: unknown[]) => void) {
-      console.log({ origin, allowedOrigins });
+      // console.log({ origin, allowedOrigins });
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
@@ -35,7 +43,8 @@ async function bootstrap() {
     },
     methods: ["GET", "HEAD", "PUT", "POST", "DELETE"],
     credentials: true,
-    allowedHeaders: "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe",
+    allowedHeaders:
+      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, x-vercel-protection-bypass, x-vercel-set-bypass-cookie",
   });
   app.useGlobalFilters(new ZodFilter());
   await app.listen(port);
