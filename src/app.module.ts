6b1b820146d3049bb828a3b6d8de88ecc510b34a
jamby77 <jamby77@gmail.com>
2025-03-08 01:51:13 +0200

    add settings.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SettingsModule } from './settings/settings.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: ['.env', '.env.local'],
     }),
+    SettingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
