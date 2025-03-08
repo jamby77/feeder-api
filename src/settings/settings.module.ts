a8014bb1d41586a4d998a6725b239c6801229423
jamby77 <jamby77@gmail.com>
2025-03-08 02:24:01 +0200

    add feeds module

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
+import { FeedsModule } from './feeds/feeds.module';
 
 @Module({
-  imports: [ConfigModule]
+  imports: [ConfigModule, FeedsModule]
 })
 export class SettingsModule {}
