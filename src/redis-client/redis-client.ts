f185a2445dd274e7549e300b1c7da5550adb5e6e
pd-redis <petar.dzhambazov@redis.com>
2025-03-13 11:16:24 +0200

    wrap context in object

--- src/redis-client/redis-client.ts
+++ src/redis-client/redis-client.ts
@@ -9,7 +9,12 @@ export class RedisClient {
   constructor() {
     const config = redisConfig();
     this.client = createClient(config);
-    this.client.on("error", err => this.logger.error("Redis Client Error", err, config));
+    this.client.on("error", err =>
+      this.logger.error("Redis Client Error", {
+        err,
+        config,
+      }),
+    );
   }
 
   async getClient() {
